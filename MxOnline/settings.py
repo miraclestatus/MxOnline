2c295ba65637fc626bc124a7d945b79032b04c2a
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-05-12 19:09:40 +0800

    课程机构和课程关联

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'MxOnline.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mxonline3', # 数据库名字
+        'NAME': 'mxonline6', # 数据库名字
         'USER':'root',
         'PASSWORD':'root',
         'HOST':'localhost',
--- apps/courses/models.py
+++ apps/courses/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 
 # Create your models here.
+from apps.organizations.models import CourseOrg
 from apps.users.models import BaseModel
 
 class Course(BaseModel):
+    course_org = models.ForeignKey(CourseOrg, null=True, blank=True, on_delete=models.CASCADE, verbose_name="课程机构")
     name = models.CharField(verbose_name="课程名", max_length=50)
     desc = models.CharField(verbose_name="课程描述", max_length=300)
     learn_times = models.IntegerField(default=0, verbose_name="学习时长（分钟数）")
