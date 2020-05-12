81e4db5f14a0d3d7bd54fabc9e818e769ab8c263
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-05-12 19:29:54 +0800

    排序

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -8,6 +8,16 @@ class CourseListView(View):
         """获取课程列表信息"""
         all_courses = Course.objects.order_by("-add_time")
 
+        sort = request.GET.get('sort', "")
+        if sort == 'students':
+            # 根据参与人数排序  减号代表倒序排序的意思
+            # li><a href="?sort=students">参与人数</a></li>
+            all_courses = all_courses.order_by('-students')
+        elif sort == 'hot':
+            # 课程排序  <li><a href="?sort=hot">最热门</a></li>
+            # 根据最热门进行排序 参与人数  <
+            all_courses = all_courses.order_by('-click_nums')
+
         # 对课程机构进行分页
         try:
             page = request.GET.get('page', 1)
@@ -18,4 +28,5 @@ class CourseListView(View):
 
         return render(request, 'course-list.html',
                       {"all_courses":courses,
+                       "sort":sort,
                        })
