b86f7f15a0ccadbf1380aa99e2422af112c82f89
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-05-12 19:40:04 +0800

    热门课程

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -7,6 +7,8 @@ class CourseListView(View):
     def get(self, request, *args, **kwargs):
         """获取课程列表信息"""
         all_courses = Course.objects.order_by("-add_time")
+        ## 获取热门课程 前3个
+        hot_courses = Course.objects.order_by("-click_nums")[:3]
 
         sort = request.GET.get('sort', "")
         if sort == 'students':
@@ -29,4 +31,5 @@ class CourseListView(View):
         return render(request, 'course-list.html',
                       {"all_courses":courses,
                        "sort":sort,
+                       "hot_courses":hot_courses,
                        })
